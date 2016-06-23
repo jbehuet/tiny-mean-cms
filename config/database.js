29100b0f250f45d0200a73b58061b0883fe35d2e
jbehuet <jbehuet@gmail.com>
2016-06-23 21:39:00 +0200

    Add default value

--- config/database.js
+++ config/database.js
@@ -4,4 +4,4 @@
 
 var mongoose = require('mongoose');
 
-module.exports = mongoose.connect(process.env.MONGODB_URI);
+module.exports = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/mean-starter');
