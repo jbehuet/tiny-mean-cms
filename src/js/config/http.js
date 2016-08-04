511bebc7b7fbe02eb4c63e8b4754360c67016faa
jbehuet <jbehuet@gmail.com>
2016-08-04 11:58:30 +0200

    Disable redirection to home if current state is login

--- src/js/config/http.js
+++ src/js/config/http.js
@@ -11,7 +11,7 @@
                 },
                 responseError(response) {
                     let state = $injector.get('$state')
-                    if (response.status === 401 || response.status === 403)
+                    if ( (response.status === 401 || response.status === 403) && state.current.name !== 'login')
                         state.go('app.home')
                     return $q.reject(response)
                 }
