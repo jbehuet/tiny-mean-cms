18ea5d6b1dc1e10625af5a6383e55f44e74d477a
jbehuet <jbehuet@gmail.com>
2016-09-04 15:42:31 +0200

    Edit route /api/post to /api/posts

--- app/routes/posts.js
+++ app/routes/posts.js
@@ -9,14 +9,14 @@ module.exports = (app) => {
 
 	let postCtrl = new PostController()
 
-	app.get('/post', (req, res, next) => { return postCtrl.findAll(req, res, next) })
+	app.get('/posts', (req, res, next) => { return postCtrl.findAll(req, res, next) })
 
-	app.get('/post/:id', (req, res, next) => { return postCtrl.findById(req, res, next) })
+	app.get('/posts/:id', (req, res, next) => { return postCtrl.findById(req, res, next) })
 
-	app.post('/post', Auth.user.isAdministrator,  (req, res, next) => { return postCtrl.create(req, res, next) })
+	app.post('/posts', Auth.user.isAdministrator,  (req, res, next) => { return postCtrl.create(req, res, next) })
 
-	app.put('/post/:id', Auth.user.isAdministrator, (req, res, next) => { return postCtrl.update(req, res, next) })
+	app.put('/posts/:id', Auth.user.isAdministrator, (req, res, next) => { return postCtrl.update(req, res, next) })
 
-	app.delete('/post/:id', Auth.user.isAdministrator, (req, res, next) => { return postCtrl.delete(req, res, next) })
+	app.delete('/posts/:id', Auth.user.isAdministrator, (req, res, next) => { return postCtrl.delete(req, res, next) })
 
 }
